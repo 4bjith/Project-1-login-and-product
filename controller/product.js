95b250f7bad2e8987d7a8f701b28c7fb459c1bf2
4bjith <abhijithpops1890@gmail.com>
2025-09-23 22:19:19 +0530

    Added update function in product controller

--- controller/product.js
+++ controller/product.js
@@ -1,76 +1,104 @@
-import express from 'express'
+import express from "express";
 
-import ProductModel from '../models/model.js'
+import ProductModel from "../models/model.js";
 
-export const GetProduct = async(req,res)=>{
-
-    // const singleItem= req.query.search;
-    // if(singleItem!=""){
-    //     const pro = await ProductModel.findOne({title:singleItem})
-    //     if(pro){
-    //         return res.json(new Array(pro))
-    //     }else{
-    //         return res.json([])
-    //     }
-    // }
-    // console.log("get req ",singleItem)
-    // const items = await ProductModel.find({})
-    // res.json(items)
-
-     try {
-        const { search } = req.query;
-
-        if (search) {
-            const product = await ProductModel.findOne({ title: search });
-
-            // Return 404 if no product is found, otherwise return the product.
-            if (!product) {
-                return res.json([]);
-            }
-            return res.json(new Array(product));
-        }
-
-        // If no search query, return all products.
-        const allItems = await ProductModel.find({}).populate("catagory").exec()
-        res.status(200).json(allItems);
-    } catch (err) {
-        console.error("Error in GetProduct:", err);
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
-
-export const PostProduct = async(req,res)=>{
+export const GetProduct = async (req, res) => {
+  // const singleItem= req.query.search;
+  // if(singleItem!=""){
+  //     const pro = await ProductModel.findOne({title:singleItem})
+  //     if(pro){
+  //         return res.json(new Array(pro))
+  //     }else{
+  //         return res.json([])
+  //     }
+  // }
+  // console.log("get req ",singleItem)
+  // const items = await ProductModel.find({})
+  // res.json(items)
 
   try {
-    const { title, price, image,catagory } = req.body;
+    const { search } = req.query;
+
+    if (search) {
+      const product = await ProductModel.findOne({ title: search });
+
+      // Return 404 if no product is found, otherwise return the product.
+      if (!product) {
+        return res.json([]);
+      }
+      return res.json(new Array(product));
+    }
+
+    // If no search query, return all products.
+    const allItems = await ProductModel.find({}).populate("catagory").exec();
+    res.status(200).json(allItems);
+  } catch (err) {
+    console.error("Error in GetProduct:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const PostProduct = async (req, res) => {
+  try {
+    const { title, price, image, catagory } = req.body;
 
     if (!title || !price || !image || !catagory) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    const newProduct = await ProductModel.create({ title, price, image ,catagory});
+    const newProduct = await ProductModel.create({
+      title,
+      price,
+      image,
+      catagory,
+    });
     res.status(201).json(newProduct);
-
   } catch (err) {
     console.error("Error creating product:", err);
     res.status(500).send("Error creating product: " + err.message);
   }
+};
 
-}
+export const DeleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-export const DeleteProduct = async(req,res) => {
-    try{
-        const {id} = req.params
+    const deleteItem = await ProductModel.findByIdAndDelete(id);
 
-        const deleteItem = await ProductModel.findByIdAndDelete(id)
-
-        if(!deleteItem){
-            return res.status(404).send("Item not found")
-        }
-
-        res.status(200).send("Item delete sucessfully. ")
-
-    }catch(err){
-        res.status(500).send(err.message)
+    if (!deleteItem) {
+      return res.status(404).send("Item not found");
     }
-}
+
+    res.status(200).send("Item delete sucessfully. ");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const UpdateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, price, image, catagory } = req.body;
+
+    //validating
+    if (!title || !price || !image || !catagory) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // find product by id and update
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      id,
+      { title, price, image, catagory },
+      { new: true, runValidators: true } // return updated doc + validate schema
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    console.error("Error updating product:", err);
+    res.status(500).json({ message: "Error updating product: " + err.message });
+  }
+};
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ import {
   DeleteProduct,
   GetProduct,
   PostProduct,
+  UpdateProduct,
 } from "../controller/product.js";
 import { LoginCheck } from "../middleware/auth.js";
 
@@ -11,5 +12,6 @@ const product = express.Router();
 product.get("/product", GetProduct);
 product.post("/product",LoginCheck, PostProduct);
 product.delete("/product/:id",LoginCheck, DeleteProduct);
+product.put("/product/:id",LoginCheck,UpdateProduct)
 
 export default product;
